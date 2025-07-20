18ae14e0d205b4b78720a723a3e95251f45e3c4a
Juyeon Sohn <xoxoeony@gmail.com>
2025-07-20 21:21:00 +0900

    Add files via upload

---
+++ player.js
@@ -0,0 +1,21 @@
+ document.addEventListener('DOMContentLoaded', () => {
+ const projectCode = "sample";
+
+    ttContainer.mqttConnect(projectCode, TOPIC_TYPE.DISPLAY, function () {
+      console.log("MQTT 연결 성공 (DISPLAY)");
+
+      ttContainer.onMessage = function (message) {
+        const url = message.trim();
+  const video = document.getElementById("player");
+  video.src = url;      // 직접 src에
+  video.load();
+  video.play()
+       .then(() => { video.muted = false; })
+        .catch(err => console.error('자동 재생 차단됨:', err));
+      };
+    },
+    {
+brokerUrl: "wss://broker.hivemq.com:8000/mqtt"
+  }
+    );
+});
